9b8a63356c5fdbe41dc0111a4c6f583e7e1f5221
xinnan cao <xinnancao@gmail.com>
2022-10-31 21:52:38 -0400

    added js

--- Develop/script.js
+++ Develop/script.js
@@ -3,23 +3,23 @@
 var generateBtn = document.querySelector("#generate");
 var strongpw = "1234567890qwertyuiopasdfghjklzxcvbnm`~!@#$%^&*()-=_+QWERTYUIOP[]{}\|ASDFGHJKL:'ZXCVBNM<>?,./";
 var easypw = "1234567890qwertyuiopasdfghjklzxcvbnm";
+var password="";
 // Write password to the #password input
 function writePassword() {
-  // var password = generatePassword();
   var passwordText = document.querySelector("#password");
   var userChoiceL=window.prompt("Length of the Password");
   if (!userChoiceL) {
     return;}
-  else if (userChoiceL>8 && userChoiceL<128){
+  else if (userChoiceL>=8 && userChoiceL<=128){
   var userChoiceT=window.confirm("Whether or not to include lowercase, uppercase, numeric, and/or special characters?")
     if (userChoiceT){
-      for (var i = 0; i <= userChoiceL; i++) {
+      for (var i = 0; i <= userChoiceL-1; i++) {
         var randomNumber = Math.floor(Math.random() * strongpw.length);
         password += strongpw.substring(randomNumber, randomNumber +1);
        }
     }
     else {
-      for (var i = 0; i <= userChoiceL; i++) {
+      for (var i = 0; i <= userChoiceL-1; i++) {
         var randomNumber = Math.floor(Math.random() * easypw.length);
         password += easypw.substring(randomNumber, randomNumber +1);
        }
@@ -30,6 +30,5 @@ function writePassword() {
   }
   passwordText.value = password;
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
